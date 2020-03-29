4809ca030f5502d7993ffa8cf2b805641e71e05b
Keita Sasako <childbamboo1980@gmail.com>
2020-03-29 15:05:52 +0900

    add install.sh.

---
+++ bin/install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "# zsh setting"
+
+echo "## change defaullt shell zsh"
+
+chsh -s /usr/local/bin/zsh
+
+echo "## set links of zsh configuration files of zprezto"
+
+ln -s $DOTFILEDIR/.zprezto/runcoms/zshenv $HOME/.zshenv
+ln -s $DOTFILEDIR/.zprezto/runcoms/zprofile $HOME/.zprofile
+ln -s $DOTFILEDIR/.zprezto/runcoms/zshrc $HOME/.zshrc
+ln -s $DOTFILEDIR/.zprezto/runcoms/zpreztorc $HOME/.zpreztorc
+ln -s $DOTFILEDIR/.zprezto/runcoms/zlogin $HOME/.zlogin
+ln -s $DOTFILEDIR/.zprezto/runcoms/zlogout $HOME/.zlogout
+
+touch $HOME/.zsh_history
+
+echo "# vim setting"
+
+ln -s $DOTFILEDIR/.vimrc $HOME/.vimrc
