cd3d562b4ea103588fd4c0e827fb18a3bffd52cf
keita sasako <childbamboo1980@gmail.com>
2020-03-30 10:31:37 +0900

    add dependency pkgs for mac setup.

---
+++ bin/setup_mac.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "# install dependency pkgs to setup for mac"
+
+echo "## install homebrew"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+echo "## update homebrew"
+brew update
+
+echo "## install xcode"
+xcode-select --install
+
+echo "# deploy dotfiles"
+
+echo "## brew bundle"
+brew bundle --file $HOME/dotfiles/Brewfile
+
+echo "## brew bundle"
+$HOME/dotfiles/bin/install_dotfile.sh
